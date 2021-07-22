ef1271b13f2557e520d214c752f8dde37320acbc
Zemo <saikaungsan@ucsm.edu.mm>
2021-07-22 15:36:40 +0630

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Router from "vue-router";
+
+Vue.use(VueRouter)
+
+const Home = () => import('../view/home/Home')
+const Category = () => import('../view/category/Category')
+const Cart = () => import('../view/cart/Cart')
+const Profile = () => import('../view/profile/Profile')
+
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new Router({
+  routes,
+  mode:"history"
+})
+
+export default router
---
+++ src/view/home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>zye</h2>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
