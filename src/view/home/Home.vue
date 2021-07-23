791b76feb310eb5b8ab93238dc08325d476011ba
Zemo <saikaungsan@ucsm.edu.mm>
2021-07-23 17:11:33 +0630

    Home and banner

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <h2>zye</h2>
+ <div id="home">
+   <nav-bar class="home-nav"><div slot="center">Shopping Mall</div> </nav-bar>
+   <home-swiper :banners="banners"></home-swiper>
+   <recommend-view :recommends="recommends"/>
+ </div>
 </template>
 
 <script>
+
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "@/view/home/childrenComponent/HomeSwiper";
+import RecommendView from "@/view/home/childrenComponent/RecommendView";
+import {getHomeMultidata} from '@/network/home'
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    RecommendView,
+    HomeSwiper,
+    NavBar,
+
+  },
+  data(){
+    return{
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+
 }
 </script>
 
 <style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color: white;
+}
 
 </style>
