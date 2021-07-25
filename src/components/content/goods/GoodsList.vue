47ec3b00975606a169d1e5a385937fee03df1051
Zemo <saikaungsan@ucsm.edu.mm>
2021-07-25 09:04:17 +0630

    Home Module Complete

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+    <goods-item v-for="(item,index) in goods" :goods-item="item"/>
+  </div>
+</template>
+
+<script>
+
+import GoodsItem from "@/components/content/goods/GoodsItem";
+
+export default {
+  name: "GoodsList",
+  components: {GoodsItem},
+  props: {
+    goods: {
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  .goods{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+</style>
--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -3,6 +3,13 @@
    <nav-bar class="home-nav"><div slot="center">Shopping Mall</div> </nav-bar>
    <home-swiper :banners="banners"></home-swiper>
    <recommend-view :recommends="recommends"/>
+   <feature-view></feature-view>
+   <tab-control class="tab-control"
+                :titles="['Popular','New','Choice']"
+                @tabClick="tabClick"
+
+   />
+   <goods-list :goods="showGoods"></goods-list>
  </div>
 </template>
 
@@ -11,12 +18,20 @@
 import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "@/view/home/childrenComponent/HomeSwiper";
 import RecommendView from "@/view/home/childrenComponent/RecommendView";
-import {getHomeMultidata} from '@/network/home'
+import FeatureView from "@/view/home/childrenComponent/FeatureView";
+
+import TabControl from "@/components/content/tabControl/TabControl";
+import GoodsList from "@/components/content/goods/GoodsList";
+
+import {getHomeMultidata,getHomeGoods} from '@/network/home'
 
 
 export default {
   name: "Home",
   components: {
+    GoodsList,
+    TabControl,
+    FeatureView,
     RecommendView,
     HomeSwiper,
     NavBar,
@@ -25,23 +40,94 @@ export default {
   data(){
     return{
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0,list: []},
+        'new': {page: 0,list: []},
+        'sell': {page: 0,list: []},
+      },
+      currentType : 'pop'
     }
   },
+  computed:{
+    showGoods(){
+      return this.goods[this.currentType].list
+    }
+
+  },
   created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
+    // 1.request multidata
+    this.getHomeMultidata()
+
+    //2. request goods data
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+  },
+  methods: {
+    /**
+     *Click Event
+     */
+    tabClick(index){
+      switch (index){
+        case 0:
+          this.currentType = 'pop'
+          break
+        case 1:
+          this.currentType = 'new'
+          break
+        case 2:
+          this.currentType = 'sell'
+          break
+
+
+      }
+    },
+
+    /**
+     * Network Request
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page +1;
+      getHomeGoods(type,page).then( res => {
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page++
+      })
+    }
+
+
   }
+
 
 }
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px;
+}
 .home-nav{
   background-color: var(--color-tint);
   color: white;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+
+.tab-control{
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+
 }
 
 </style>
