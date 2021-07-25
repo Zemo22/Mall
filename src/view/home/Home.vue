50cc53ae1fd9c92653a43b69bdd298c3871d70a3
Zemo <saikaungsan@ucsm.edu.mm>
2021-07-25 21:00:39 +0630

    Home Vue Complete 25/7

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,15 +1,21 @@
 <template>
  <div id="home">
    <nav-bar class="home-nav"><div slot="center">Shopping Mall</div> </nav-bar>
-   <home-swiper :banners="banners"></home-swiper>
-   <recommend-view :recommends="recommends"/>
-   <feature-view></feature-view>
-   <tab-control class="tab-control"
-                :titles="['Popular','New','Choice']"
-                @tabClick="tabClick"
+   <scroll class="content" ref="scroll"
+           :probe-type="3" @scroll="contentScroll"
+           :pull-up-load="true" @pullingUp="loadMore"
+   >
+     <home-swiper :banners="banners"></home-swiper>
+     <recommend-view :recommends="recommends"/>
+     <feature-view></feature-view>
+     <tab-control class="tab-control"
+                  :titles="['Popular','New','Choice']"
+                  @tabClick="tabClick"
 
-   />
-   <goods-list :goods="showGoods"></goods-list>
+     />
+     <goods-list :goods="showGoods"></goods-list>
+   </scroll>
+   <back-top @click.native="backClick" v-show="isBackToTOp"></back-top>
  </div>
 </template>
 
@@ -22,6 +28,8 @@ import FeatureView from "@/view/home/childrenComponent/FeatureView";
 
 import TabControl from "@/components/content/tabControl/TabControl";
 import GoodsList from "@/components/content/goods/GoodsList";
+import Scroll from "@/components/common/scroll/Scroll";
+import BackTop from "@/components/content/backTop/BackTop";
 
 import {getHomeMultidata,getHomeGoods} from '@/network/home'
 
@@ -29,6 +37,8 @@ import {getHomeMultidata,getHomeGoods} from '@/network/home'
 export default {
   name: "Home",
   components: {
+    BackTop,
+    Scroll,
     GoodsList,
     TabControl,
     FeatureView,
@@ -46,7 +56,8 @@ export default {
         'new': {page: 0,list: []},
         'sell': {page: 0,list: []},
       },
-      currentType : 'pop'
+      currentType: 'pop',
+      isBackToTOp: false,
     }
   },
   computed:{
@@ -56,6 +67,7 @@ export default {
 
   },
   created() {
+
     // 1.request multidata
     this.getHomeMultidata()
 
@@ -65,9 +77,12 @@ export default {
     this.getHomeGoods('sell')
   },
   methods: {
+
     /**
      *Click Event
      */
+
+    //Click event on Tab Control
     tabClick(index){
       switch (index){
         case 0:
@@ -79,9 +94,23 @@ export default {
         case 2:
           this.currentType = 'sell'
           break
-
-
       }
+    },
+
+    //Click event for back to top
+    backClick(){
+      this.$refs.scroll.scrollTo(0,0);
+    },
+
+    //Show event for back top button
+    contentScroll(position){
+      this.isBackToTOp = -(position.y) > 1000
+    },
+
+    //Pull up event for loading more
+    loadMore(){
+      this.getHomeGoods(this.currentType)
+      this.$refs.scroll.scroll.refresh();
     },
 
     /**
@@ -99,6 +128,7 @@ export default {
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page++
       })
+      this.$refs.scroll.finishPullUp()
     }
 
 
@@ -111,6 +141,7 @@ export default {
 <style scoped>
 #home{
   padding-top: 44px;
+  height: 100vh;
 }
 .home-nav{
   background-color: var(--color-tint);
@@ -127,7 +158,20 @@ export default {
   position: sticky;
   top: 44px;
   z-index: 9;
+}
 
+/*.content{*/
+/*  height: calc(100% - 93px);*/
+/*  !*margin-top: 43px;*!*/
+/*}*/
+
+.content{
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
 }
 
 </style>
